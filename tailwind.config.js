1c78400db879174c11621bf2cf3520ff3eff4a6c
Christian Gil <christian@chrisgvdev.com>
2022-04-18 14:31:05 -0500

    chore: Update all deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/App.vue',
+    './src/pages/**/*.{vue,js,ts,jsx,tsx}',
+    './src/layouts/**/*.{vue,js,ts,jsx,tsx}',
+    './src/components/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   darkMode: 'media',
   theme: { extend: {} },
   plugins: [require('@tailwindcss/forms')],
